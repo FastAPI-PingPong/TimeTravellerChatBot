c651c61d0dfe7e123db07b49ba99a5e1bcf3729e
SeungminAhn <smahn9123@gmail.com>
2025-01-09 10:17:14 +0900

    ✨ add URL endpoint for signup

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import get_db
+from auth import get_hashed_password
+from schemas import UserCreate, UserCreateResposne
+from models import User
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+
+@app.post("/signup", response_model=UserCreateResposne)
+async def signup(user: UserCreate, db: Session = Depends(get_db)):
+    if db.query(User).filter(User.username == user.username).first():
+        raise HTTPException(status_code=409, detail="Username is already taken.")
+    hashed_password = get_hashed_password(user.password)
+    new_user = User(username=user.username, hashed_password=hashed_password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
