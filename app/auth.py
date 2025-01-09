2086f3ec62dd7c9946bbeb27fabd54b5a63ddbcd
SeungminAhn <smahn9123@gmail.com>
2025-01-09 12:24:04 +0900

    ✨ add URL endpoint for login

--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,26 @@
 from passlib.context import CryptContext
+from datetime import datetime, timezone, timedelta
+from jose import jwt
+
+SECRET_KEY = "secret_key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
 
 
 def get_hashed_password(password: str):
     return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_token(username: str):
+    encode_data = {
+        "sub": username,
+        "exp": datetime.now(timezone.utc)
+        + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES),
+    }
+    return jwt.encode(encode_data, SECRET_KEY, algorithm=ALGORITHM)
