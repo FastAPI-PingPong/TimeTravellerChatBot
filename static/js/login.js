2f2bf60f08040b1695591261ce1a6a366cbd3894
SeungminAhn <smahn9123@gmail.com>
2025-01-10 23:12:30 +0900

    ✨ implement login in login.js

--- static/js/login.js
+++ static/js/login.js
@@ -1,28 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.querySelector('.login-form');
 
-
-    const getUsers = () => {
-        const users = localStorage.getItem('users');
-        return users ? JSON.parse(users) : [];
-    };
-
-    loginForm.addEventListener('submit', (e) => {
+    loginForm.addEventListener('submit', async (e) => {
         e.preventDefault();
 
         const id = document.getElementById('id').value;
         const password = document.getElementById('password').value;
 
-        const users = getUsers();
-        const user = users.find(user => user.id === id && user.password === password);
+        try {
+            const formData = new FormData();
+            formData.append("username", id);
+            formData.append("password", password);
 
-        if (user) {
-            localStorage.setItem('currentUser', JSON.stringify(user));
-            alert('로그인 성공!');
+            const response = await fetch('http://127.0.0.1:8000/login', {
+                method: 'POST',
+                body: formData
+            });
 
-            window.location.href = 'main.html';
-        } else {
-            alert('아이디 또는 비밀번호가 일치하지 않습니다.');
+            if (response.ok) {
+                const data = await response.json();
+                localStorage.setItem('token', data.access_token);
+                alert('로그인 성공!');
+                window.location.href = 'main.html';
+            } else {
+                alert('아이디 또는 비밀번호가 일치하지 않습니다.');
+            }
+        } catch (error) {
+            alert('서버 연결 중 오류가 발생했습니다.');
+            console.error('Error:', error);
         }
     });
 });
