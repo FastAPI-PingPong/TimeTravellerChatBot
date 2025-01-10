7477ba1ef9efcb7b14557f060f162260db793950
SeungminAhn <smahn9123@gmail.com>
2025-01-10 12:04:00 +0900

    ✨ use orm.py for ORM actions

--- app/chat.py
+++ app/chat.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
+
 from .models import SessionModel, ChatModel
+from .orm import ORM
 import requests
 
 URL_ENDPOINT = "https://open-api.jejucodingcamp.workers.dev/"
@@ -51,7 +53,7 @@ class ChatManager:
 
     def __init__(self, session_id, db):
         self.session_id = session_id
-        self.db = db
+        self.orm = ORM(db)
         self.url_endpoint = URL_ENDPOINT
         self.chat_history = self.make_chat_history()
 
@@ -60,26 +62,25 @@ class ChatManager:
         Session에 속한 모든 Chat의 질문과 대답을 Message로 변환하여 chat history에 통합하여 저장.
         모든 신규 질문 요청 전에 반드시 호출되어야 함.
         """
-        session: SessionModel = (
-            self.db.query(SessionModel)
-            .filter(SessionModel.id == self.session_id)
-            .first()
-        )
-        # if not session:
-        #     pass
-        chat_list: list[ChatModel] = session.chats
+        session = self.orm.get_session_by_id(self.session_id)
         chat_history = ChatHistory()
-        if len(chat_list) > 0:
-            chat_history.append(
-                Message(role="system", content=str(chat_list[0].question))
-            )
-            chat_history.append(
-                Message(role="assistant", content=str(chat_list[0].answer))
-            )
-            for chat in chat_list[1:]:
-                print(f"[Q] {chat.question} [A] {chat.answer}")
-                chat_history.append(Message(role="user", content=str(chat.question)))
-                chat_history.append(Message(role="assistant", content=str(chat.answer)))
+        if session:
+            chat_list = session.chats
+            if len(chat_list) > 0:
+                chat_history.append(
+                    Message(role="system", content=str(chat_list[0].question))
+                )
+                chat_history.append(
+                    Message(role="assistant", content=str(chat_list[0].answer))
+                )
+                for chat in chat_list[1:]:
+                    print(f"[Q] {chat.question} [A] {chat.answer}")
+                    chat_history.append(
+                        Message(role="user", content=str(chat.question))
+                    )
+                    chat_history.append(
+                        Message(role="assistant", content=str(chat.answer))
+                    )
         return chat_history
 
     def send_question_with_history(self):
@@ -99,14 +100,6 @@ class ChatManager:
         answer = self.send_question_with_history()
         return answer
 
-    def create_new_chat(self, question, answer):
-        """ChatGPT의 대답 이후, 질문과 대답을 DB에 저장하기 위한 함수"""
-        new_chat = ChatModel(
-            session_id=self.session_id, question=question, answer=answer
-        )
-        self.db.add(new_chat)
-        self.db.commit()
-
     def get_introduction(self):
         """ChatGPT가 수행할 역할을 설정하고, 자기소개 멘트를 확보하기"""
 
@@ -117,18 +110,14 @@ class ChatManager:
             짧게 너의 소개를 부탁해."""
             return prompt
 
-        session: SessionModel = (
-            self.db.query(SessionModel)
-            .filter(SessionModel.id == self.session_id)
-            .first()
-        )
+        session = self.orm.get_session_by_id(self.session_id)
         introduction_prompt = _make_introduction_prompt_message(
             year=session.year, location=session.location, persona=session.persona
         )
         introduction = self.add_question_into_history_and_get_answer(
             role="system", question=introduction_prompt
         )
-        self.create_new_chat(question=introduction_prompt, answer=introduction)
+        self.orm.create_chat(self.session_id, introduction_prompt, introduction)
         return introduction
 
     def get_answer(self, question):
@@ -137,5 +126,5 @@ class ChatManager:
         answer = self.add_question_into_history_and_get_answer(
             role="user", question=question
         )
-        self.create_new_chat(question=question, answer=answer)
+        self.orm.create_chat(self.session_id, question, answer)
         return answer
